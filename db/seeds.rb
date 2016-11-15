fb9d0a6f60442efe655badad2a437a73c964a8fb
Jim Rohrer <jim.rohrer@gmail.com>
2016-11-15 10:40:07 -0800

    Assignment 17 - Seeding Data: idempotent adds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,16 @@ posts = Post.all
   )
 end
 
+p = Post.find_or_create_by!(
+  title: "This unique title of a post",
+  body: "This unique body of a post is very short."
+)
+
+Comment.find_or_create_by!(
+  body: "Here's a unique comment body.",
+  post_id: p.id
+)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
