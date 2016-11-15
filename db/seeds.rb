b6b52d893360a3750d6b6dbe91f38656906ed1a0
Jim Rohrer <jim.rohrer@gmail.com>
2016-11-14 22:00:21 -0800

    Ckpt 17 - Seeding Data: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body:  RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
