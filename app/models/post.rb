91a713487aedb0b99f578c825e684cc5cec79a33
Jim Rohrer <jim.rohrer@gmail.com>
2016-11-30 14:02:32 -0800

    Assignment 27 - unscope

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ApplicationRecord
 
   default_scope { order('created_at DESC') }
 
+  scope :ordered_by_title, -> { order('title ASC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC').unscope(:order) }
+  
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
