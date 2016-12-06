976f39794697e31604a41c73b5bfdd0fae23f1af
Jim Rohrer <jim.rohrer@gmail.com>
2016-12-06 15:17:22 -0800

    Assignment-30-auto-upvote

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  after_create :create_vote
 
   default_scope { order('rank DESC') }
 
@@ -27,6 +28,13 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 
 end
