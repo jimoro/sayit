579c71cab76fc00ae7c8db21500fff7a4e97df32
Jim Rohrer <jim.rohrer@gmail.com>
2016-12-08 14:56:49 -0800

    Assignment 31 - Auto-Fave w/email notification for new posts

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,16 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, cc: "michaelcpell@gmail.com", subject: "new comment on #{post.title}")
   end
+
+  def new_post(post)
+
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+
+    mail(to: post.user.email, cc: "michaelcpell@gmail.com", subject: "Auto-Favorited your new Bloccit post of #{post.title}.")
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :favorite_new_post
 
   default_scope { order('rank DESC') }
 
@@ -28,6 +29,11 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def favorite_new_post
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 
 end
